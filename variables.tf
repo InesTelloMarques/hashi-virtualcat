0d876dd314665a896738aa2551f4b7a38710a76d
Inés Tello Marqués <173058694+InesTelloMarques@users.noreply.github.com>
2024-07-18 09:58:52 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "instance_type" {
   description = "Specifies the AWS instance type."
   type        = string
-  default     = "t2.micro"
 }
 
 variable "region" {
   description = "Specifies the AWS region."
   type        = string
-  default     = "us-east-2"
 }
